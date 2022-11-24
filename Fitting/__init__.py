a34ca963ad3635fc54e502f61ed58a404fa28305
ludgerradke <radke@intervall.io>
2022-11-24 19:28:08 +0100

    gitignore updated

--- Fitting/__init__.py
+++ Fitting/__init__.py
@@ -2,3 +2,4 @@ from Fitting.T1 import InversionRecoveryT1
 from Fitting.T1rho import T1rho
 from Fitting.T2prep import T2prep
 from Fitting.T2_T2star import T2_T2star
+from Fitting.FittingMap import FittedMap
