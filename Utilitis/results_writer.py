800d6642300cbccb3ca1349b4e5f98b51264e488
ludgerradke <radke@intervall.io>
2022-11-24 19:28:59 +0100

    Results_writer made more readable.

--- Utilitis/results_writer.py
+++ Utilitis/results_writer.py
@@ -16,31 +16,28 @@ def save_results(fit_map: np.ndarray | None,
     nii_folder.mkdir(parents=True, exist_ok=True)
 
     save_nii(fit_map, affine, header, nii_folder / 'map.nii.gz')
-    save_nii(r2, affine, header, nii_folder / 'r2.nii.gz')
+    if r2 is not None:
+        save_nii(r2, affine, header, nii_folder / 'r2.nii.gz')
 
     results = {}
     for i in range(1, int(mask.max()) + 1):
         m = mask.copy()
         m = np.where(m == i, 1, 0)
 
-        fit_map = np.multiply(fit_map, m)
-        k = fit_map.copy()
-        k[k > 0] = 1
-        fit_map = np.where(fit_map != 0.0, fit_map, np.nan)
+        times = fit_map[m == 1]
+        if len(times) == 0:
+            continue
 
-        r_squares = np.multiply(self.r_squares, m)
-        r_squares = np.where(r_squares != 0, r_squares, np.nan)
-
-        results[str(i)] = ['%.2f' % np.nanmean(fit_map), '%.2f' % np.nanstd(fit_map),
-                           '%.2f' % np.nanmin(fit_map), '%.2f' % np.nanmax(fit_map),
-                           '%.2f' % np.nansum(k) + '/' + '%.2f' % np.sum(m),
-                           '%.2f' % np.nanmean(r_squares)]
-        with open(result_file, mode='w', newline='') as csv_file:
-            writer = csv.writer(csv_file, delimiter=';')
-            writer.writerow(['mask_index', 'mean', 'std', 'min', 'max', 'Pixels', 'Mean R^2'])
-            for key, value in results.items():
-                value = [v.replace('.', decimal) for v in value]
-                writer.writerow([key] + value)
+        results[str(i)] = ['%.2f' % np.nanmean(times), '%.2f' % np.nanstd(times),
+                           '%.2f' % np.nanmin(times), '%.2f' % np.nanmax(times),
+                           '%.2f' % len(times[~np.isnan(times)]) + '/' + '%.2f' % np.sum(m),
+                           '%.2f' % np.nanmean(r2[m == 1]) if r2 is not None else 'NaN']
+    with open(result_file, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=';')
+        writer.writerow(['mask_index', 'mean', 'std', 'min', 'max', 'Pixels', 'Mean R^2'])
+        for key, value in results.items():
+            value = [v.replace('.', decimal) for v in value]
+            writer.writerow([key] + value)
     return results
 
 
